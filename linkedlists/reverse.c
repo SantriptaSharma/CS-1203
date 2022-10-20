7424fbed887ec80a6798a1a32fdc734eb8e2b347
SantriptaSharma <santripta@gmail.com>
2022-10-20 08:23:03 +0530

    Complete reversed

--- linkedlists/reverse.c
+++ linkedlists/reverse.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include "linkedlist.h"
 
 int main()
 {
-    printf("hi");
+    int N;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &N);
+
+    Node *head = NULL;
+    
+    for (int i = 0; i < N; i++)
+    {
+        int el;
+
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &el);
+        Insert(&head, el);
+    }
+
+    printf("Created linked list: ");
+    PrintList(head);
+
+    ReverseList(&head);
+    printf("Reversed: ");
+    PrintList(head);
+    FreeList(head);
 }
