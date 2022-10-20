01a05638cf5fc09c705964fd28c673a608f6d11e
SantriptaSharma <santripta@gmail.com>
2022-10-20 08:21:12 +0530

    Complete from integers

--- linkedlists/from_integers.c
+++ linkedlists/from_integers.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "linkedlist.h"
 
 int main()
 {
-    printf("hello world");
+    int N;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &N);
+
+    Node *head = NULL;
+    
+    for (int i = 0; i < N; i++)
+    {
+        int el;
+
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &el);
+        Insert(&head, el);
+    }
+
+    printf("Created linked list: ");
+    PrintList(head);
+    FreeList(head);
 }
